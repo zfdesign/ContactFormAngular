dd762c5174f26b5a2f79de229f497d0d031a7217
zlatin.ivanov <zlatin.ivanov@just-eat.com>
2015-10-04 12:35:11 +0100

    Adding form submit response

--- ContactFormAngular/Controllers/ContactController.cs
+++ ContactFormAngular/Controllers/ContactController.cs
@@ -8,9 +8,17 @@ namespace ContactFormAngular.Controllers
 {
     public class ContactController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            var contactForm = new Models.ContactFormAngular();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactFormAngular contactForm)
+        {
+            return View("ThankYou", contactForm);
         }
     }
 }
