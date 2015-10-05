9544f7922bc166ca0d845e06874d8fd670fde3a9
zlatin.ivanov <zlatin.ivanov@just-eat.com>
2015-10-05 03:36:15 +0100

    Refactor app.js

--- ContactFormAngular/Scripts/app.js
+++ ContactFormAngular/Scripts/app.js
@@ -1,27 +1,49 @@
-﻿(function (angular) {
+﻿'use strict';
+var app = angular.module('app', []);
 
-    angular.module('app', []).controller("ContactFormController", ContactFormController);
-
-    function ContactFormController() {
-        this.title = 'Contact form';
+app.controller("ContactFormController", [
+    '$scope', '$http', '$httpParamSerializerJQLike', function ($scope) {
         this.enquiry = {
             "name": "",
             "email": "",
             "comment": ""
         };
+        this.controllerScope = $('#contact-form-controller');
         this.newEnquiry = angular.copy(this.enquiry);
         this.formSubmitted = false;
+
+        this.submit = function (form) {
+            if (form.$valid) {
+                this.enquiry = angular.copy(this.newEnquiry);
+                /*var that = this;
+                $http({
+                    method: "POST",
+                    url: form.action,
+                    data: $httpParamSerializerJQLike(this.enquiry)
+                }).then(function(response) {
+                    console.log('Sucess: ' + response);
+                    that.formSubmitted = true;
+                }, function(error) {
+                    console.log('Error: ' + error);
+                });*/
+                this.formSubmitted = true;
+            }
+        };
+        this.reset = function () {
+            this.newEnquiry = angular.copy(this.enquiry);
+            this.resetValidationErrors();
+        };
+
+        this.resetValidationErrors = function () {
+            this.controllerScope.find('span.text-danger span').remove();
+        };
+
+        this.disableFormSubmition = function () {
+            this.controllerScope.on('submit', 'form', function (e) {
+                e.preventDefault();
+            });
+        };
+        this.disableFormSubmition();
+
     }
-
-    ContactFormController.prototype.submit = function (contactForm) {
-        console.log(contactForm);
-        if (contactForm.$valid) {
-            this.enquiry = angular.copy(this.newEnquiry);
-            this.formSubmitted = true;
-        }
-    };
-    ContactFormController.prototype.reset = function () {
-        console.log(contactForm);
-    };
-
-})(window.angular);
+]);
